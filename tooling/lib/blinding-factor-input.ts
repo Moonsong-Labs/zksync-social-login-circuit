6f156695c475494b7af452ae8bc2edfa41e11540
Miguel Duarte <miguelfeliped@gmail.com>
2025-02-18 07:06:09 -0300

    Update tooling/lib/blinding-factor-input.ts
    Co-authored-by: Matías Ignacio González <maigonzalez@fi.uba.ar>

--- tooling/lib/blinding-factor-input.ts
+++ tooling/lib/blinding-factor-input.ts
@@ -21,7 +21,6 @@ export class BlindingFactorInputTest implements CircuitInput<BlindingFactorInput
     const nonce = createNonce(this.txHash, this.blindingFactor);
     const txHash = ByteVector.fromHex(this.txHash);
 
-    // const b64Nonce = ByteVector.fromAsciiString(nonce).padRight("=".charCodeAt(0), 44).toCircomByteArray();
 
     const b64Nonce = ByteVector.fromAsciiString(nonce).padRight(0, MAX_B64_NONCE_LENGTH).toCircomByteArray();
 
