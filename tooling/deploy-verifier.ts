b80b2645ecd8345bb5c6c728d2af95037b7f7be6
calvo.generico <miguelfeliped@gmail.com>
2025-03-10 10:37:18 -0300

    improve deploy and call verifier commands

--- tooling/deploy-verifier.ts
+++ tooling/deploy-verifier.ts
@@ -4,14 +4,18 @@ import path from "node:path";
 
 import { defaultVerifierPath } from "./generate-verifier.js";
 import { cmd, ROOT_DIR } from "./lib/cmd.js";
+import { envOrDefault } from "./lib/env.js";
 
 export async function deployVerifier(circuit: string): Promise<void> {
   const data = path.parse(circuit);
 
-  const rpcUrl = "http://localhost:8011";
-  const privKey = "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"; // third rich account
+  const rpcUrl = envOrDefault("RPC_URL", "http://localhost:8011");
+  const privKey = envOrDefault(
+    "SEC_KEY",
+    "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a");
   const verifier = defaultVerifierPath(data.name);
   // console.log(verifier);
   await cmd(`forge create --rpc-url ${rpcUrl} --private-key ${privKey} --zksync ${ROOT_DIR}/${verifier}:Groth16Verifier`);
+  // await cmd(`forge create --rpc-url ${rpcUrl} --private-key ${privKey} ${ROOT_DIR}/${verifier}:Groth16Verifier --broadcast`);
   // await cmd(`cat ${verifier}`);
 }
