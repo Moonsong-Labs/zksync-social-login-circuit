5ab6851cebe7bf9301024923b82e2d9ecd1cab73
Miguel Duarte <miguelfeliped@gmail.com>
2025-03-11 11:29:10 -0300

    Update tooling/deploy-verifier.ts
    Co-authored-by: Matías Ignacio González <maigonzalez@fi.uba.ar>

--- tooling/deploy-verifier.ts
+++ tooling/deploy-verifier.ts
@@ -14,8 +14,6 @@ export async function deployVerifier(circuit: string): Promise<void> {
     "SEC_KEY",
     "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a");
   const verifier = defaultVerifierPath(data.name);
-  // console.log(verifier);
+
   await cmd(`forge create --rpc-url ${rpcUrl} --private-key ${privKey} --zksync ${ROOT_DIR}/${verifier}:Groth16Verifier`);
-  // await cmd(`forge create --rpc-url ${rpcUrl} --private-key ${privKey} ${ROOT_DIR}/${verifier}:Groth16Verifier --broadcast`);
-  // await cmd(`cat ${verifier}`);
 }
