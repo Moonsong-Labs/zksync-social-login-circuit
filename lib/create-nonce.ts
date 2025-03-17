20295d07755b1afeb388c4cbdc3e62dec4c87c43
matias-gonz <maigonzalez@fi.uba.ar>
2025-03-17 10:45:45 -0300

    add create-nonce

--- lib/create-nonce.ts
+++ lib/create-nonce.ts
@@ -1,10 +1,29 @@
 import { poseidon3 } from "poseidon-lite";
+import { type Address, encodeAbiParameters, keccak256 } from "viem";
 
-import { ByteVector } from "./byte-vector.js";
+import { ByteVector, type Hex } from "./byte-vector.js";
 
 export function createNonce(txHashHex: string, blindingFactor: bigint): string {
   // Padding to complete 2 fields
   const nonceFields = ByteVector.fromHex(txHashHex).padRight(0, 62).toFieldArray();
   const hash = poseidon3([...nonceFields, blindingFactor]);
   return ByteVector.fromBigInt(hash).toBase64Url();
+}
+
+export function createNonceV2(address: Address, contractNonce: bigint, blindingFactor: bigint): [Hex, string] {
+  const encoded = encodeAbiParameters(
+    [
+      {
+        type: "address",
+      },
+      {
+        type: "uint256",
+      },
+    ],
+    [address, contractNonce],
+  );
+  const senderHash = keccak256(encoded);
+  const nonceFields = ByteVector.fromHex(senderHash).padRight(0, 62).toFieldArray();
+  const hash = poseidon3([...nonceFields, blindingFactor]);
+  return [senderHash, ByteVector.fromBigInt(hash).toBase64Url()];
 }
