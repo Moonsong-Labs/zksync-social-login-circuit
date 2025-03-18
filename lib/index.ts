aa071b61b6e9fe1eedd6ba234a3e1cfa2401bcf3
calvo.generico <miguelfeliped@gmail.com>
2025-03-18 11:40:14 -0300

    feat: recovery flow working

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 export { ByteVector } from "./byte-vector.js";
 export { CircomBigInt } from "./circom-big-int.js";
-export { createNonce } from "./create-nonce.js";
+export { createNonce, createNonceV2 } from "./create-nonce.js";
 export { JWT } from "./jwt.js";
 export { type JwtTxValidationData, JwtTxValidationInputs } from "./jwt-tx-validation-input.js";
 export { OidcDigest } from "./oidc-digest.js";
