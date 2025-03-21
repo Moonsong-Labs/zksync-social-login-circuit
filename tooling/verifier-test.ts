48d367eb2babec673f7d43675a51ff1679248473
calvo.generico <miguelfeliped@gmail.com>
2025-03-21 09:02:04 -0300

    feat: command to export test verifier.

---
+++ tooling/verifier-test.ts
@@ -0,0 +1,16 @@
+import path from "node:path";
+
+import { cmd, cmdArgs } from "./lib/cmd.js";
+import { rawZkeyFilePath } from "./zkey.js";
+
+export function testVerifierPath(name: string): string {
+  return `target/${name}/verifier-test.sol`;
+}
+
+export async function verifierTestCmd(circuit: string) {
+  const fileData = path.parse(circuit);
+  const outPath = testVerifierPath(fileData.name);
+  const zkey = rawZkeyFilePath(fileData.name);
+  await cmd(`snarkjs zkey export solidityverifier ${zkey} ${outPath}`);
+  await cmdArgs("sed", ["-i", "s/contract Groth16Verifier/contract Groth16VerifierTest/g", outPath]);
+}
